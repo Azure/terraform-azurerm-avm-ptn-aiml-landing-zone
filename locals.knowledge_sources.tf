16b3f85e3164c4175dbba945aa000b437ae9bb6c
Jon Chancellor <jchancellor@microsoft.com>
2025-06-27 14:30:32 -0700

    ai search suffix

--- locals.knowledge_sources.tf
+++ locals.knowledge_sources.tf
@@ -1,3 +1,3 @@
 locals {
-  ks_ai_search_name = try(var.ks_ai_search_definition.name, null) != null ? var.ks_ai_search_definition.name : (try(var.name_prefix, null) != null ? "${var.name_prefix}-ks-ai-search" : "ai-alz-ks-ai-search")
+  ks_ai_search_name = try(var.ks_ai_search_definition.name, null) != null ? var.ks_ai_search_definition.name : (try(var.name_prefix, null) != null ? "${var.name_prefix}-ks-ai-search" : "ai-alz-ks-ai-search-${random_string.name_suffix.result}")
 }
