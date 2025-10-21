12d565fc57f4ea27638b8fabfcc1365d317251cb
Albin Lonnfalt <albin.lonnfalt@hotmail.com>
2025-10-21 08:39:44 +0200

    Update default config example

--- examples/default-byo-vnet/variables.tf
+++ examples/default-byo-vnet/variables.tf
@@ -31,3 +31,11 @@ The name of the resource group that contains the existing Virtual Network.
 For more information about Azure Resource Groups see <https://learn.microsoft.com/en-us/azure/azure-resource-manager/management/>.
 DESCRIPTION
 }
+
+variable "subscription_id" {
+  type       = string
+  description = <<DESCRIPTION
+The ID of the subscription that contains the existing Virtual Network.
+For more information about Azure Subscriptions see <https://learn.microsoft.com/en-us/azure/cost-management-billing/manage/understand-subscriptions>.
+DESCRIPTION
+}
